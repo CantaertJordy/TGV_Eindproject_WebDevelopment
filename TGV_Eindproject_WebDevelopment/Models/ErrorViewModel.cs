938be5ae326680857c6fa38195495cc3728a17b9
Jordy Cantaert <jordy.cantaert@student.vives.be>
2019-04-08 10:39:47 +0200

    Add project files.

---
+++ TGV_Eindproject_WebDevelopment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TGV_Eindproject_WebDevelopment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
