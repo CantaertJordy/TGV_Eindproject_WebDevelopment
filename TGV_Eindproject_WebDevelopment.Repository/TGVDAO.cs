9a550bda881d46efea375506b69e9f46b7725227
Jochen Schotte <jochen.schotte@student.vives.be>
2019-05-06 20:22:45 +0200

    Added TGVDAO

---
+++ TGV_Eindproject_WebDevelopment.Repository/TGVDAO.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TGV_Eindproject_WebDevelopment.Domain.Entities;
+
+namespace TGV_Eindproject_WebDevelopment.Repository
+{
+    public class TGVDAO
+    {
+
+        #region DatabaseContext
+
+        private readonly TGVsContext _dbContext;
+
+        #endregion
+
+        #region Constructors
+
+        public TGVDAO()
+        {
+            _dbContext = new TGVsContext();
+        }
+
+        #endregion
+
+        #region Select-Statements
+
+        public IEnumerable<Tgvs> All()
+        {
+            return _dbContext.Tgvs.Include(tgv => tgv.LineNavigation).ToList();
+        }
+
+        public IEnumerable<Tgvs> TGVsOnLine(int lineId)
+        {
+            return _dbContext.Tgvs.Where(tgv => tgv.Line == lineId).ToList();
+        }
+
+        public Tgvs Get(int id)
+        {
+            return _dbContext.Tgvs.Where(tgv => tgv.Id == id).First();
+        }
+
+        #endregion
+
+    }
+}
