fd4cb61fee188754ad900e820777836997c14d27
Jochen Schotte <jochen.schotte@student.vives.be>
2019-05-06 20:10:59 +0200

    Added LineDAO

---
+++ TGV_Eindproject_WebDevelopment.Repository/LineDAO.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TGV_Eindproject_WebDevelopment.Domain.Entities;
+
+namespace TGV_Eindproject_WebDevelopment.Repository
+{
+    public class LineDAO
+    {
+
+        #region DatabaseContext
+
+        private readonly TGVsContext _dbContext;
+
+        #endregion
+
+        #region Constructors
+
+        public LineDAO()
+        {
+            _dbContext = new TGVsContext();
+        }
+
+        #endregion
+
+        #region Select-Methods
+
+        public IEnumerable<Lines> All()
+        {
+            return _dbContext.Lines.Include(line => line.DepartureNavigation).Include(line => line.DestinationNavigation).ToList();
+        }
+
+        public Lines Get(int departureId, int destinationId)
+        {
+            return _dbContext.Lines.Where(line => line.Departure == departureId && line.Destination == destinationId).FirstOrDefault();
+        }
+
+        #endregion
+
+    }
+}
