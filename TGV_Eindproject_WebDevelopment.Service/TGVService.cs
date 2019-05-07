ca7590512f4c2d4e293448f68e3b2c877145ee35
Jochen Schotte <jochen.schotte@student.vives.be>
2019-05-07 18:44:34 +0200

    Start of implementation TGVService

---
+++ TGV_Eindproject_WebDevelopment.Service/TGVService.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TGV_Eindproject_WebDevelopment.Domain.Entities;
+using TGV_Eindproject_WebDevelopment.Repository;
+
+namespace TGV_Eindproject_WebDevelopment.Service
+{
+    public class TGVService
+    {
+
+        #region Repositories
+
+        private readonly TGVDAO tgvDAO;
+
+        #endregion
+
+        #region Services
+
+        private readonly LineService lineService;
+
+        #endregion
+
+        #region Constructors
+
+        public TGVService()
+        {
+            tgvDAO = new TGVDAO();
+            lineService = new LineService();
+        }
+
+        #endregion
+
+        #region Get-Methods
+
+        public IList<Tgvs> GetJourney(int departureId, int destinationId, TimeSpan timeOfDeparture)
+        {
+            IList<Lines> route = lineService.GetRoute(departureId, destinationId);
+
+            IList<Tgvs> journey = new List<Tgvs>();
+
+            foreach (Lines l in route)
+            {
+                // verdere implementatie voor GetJourney
+            }
+
+            return journey;
+        }
+
+        public Tgvs GetJourney(Lines line, TimeSpan timeOfDeparture)
+        {
+            IEnumerable<Tgvs> tgvs = GetWithLine(line.Id);
+            Tgvs bestTgv = BestTgv(tgvs, timeOfDeparture);
+
+            if (bestTgv == null)
+                return EarliestTgv(tgvs);
+            else
+                return bestTgv;
+        }
+
+        public IEnumerable<Tgvs> GetWithLine(int lineId)
+        {
+            return tgvDAO.GetWithLine(lineId);
+        }
+
+        #endregion
+
+        #region Helper-Methods
+
+        private Tgvs EarliestTgv(IEnumerable<Tgvs> tgvs)
+        {
+            TimeSpan earliestTime = new TimeSpan(24, 0, 0);
+            Tgvs earliestTgv = null;
+
+            foreach (Tgvs t in tgvs)
+            {
+                if (earliestTime.CompareTo(t.TimeOfDeparture) > 0)
+                    earliestTgv = t;
+            }
+
+            return earliestTgv;
+        }
+
+        private Tgvs BestTgv(IEnumerable<Tgvs> tgvs, TimeSpan time)
+        {
+            Tgvs bestTgv = null;
+
+            foreach (Tgvs tgv in tgvs)
+            {
+                if ((bestTgv == null && tgv.TimeOfDeparture.CompareTo(time) >= 0) || (bestTgv != null && tgv.TimeOfDeparture.CompareTo(bestTgv.TimeOfDeparture) > 0))
+                    bestTgv = tgv;
+            }
+
+            return bestTgv;
+        }
+
+        #endregion
+
+    }
+}
