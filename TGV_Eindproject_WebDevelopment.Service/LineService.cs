ec848510f7ea3063195414ea613015f2b5773c7a
Jochen Schotte <jochen.schotte@student.vives.be>
2019-05-07 17:22:14 +0200

    Finishing GetRoute in LineService

--- TGV_Eindproject_WebDevelopment.Service/LineService.cs
+++ TGV_Eindproject_WebDevelopment.Service/LineService.cs
@@ -38,17 +38,47 @@ namespace TGV_Eindproject_WebDevelopment.Service
 
         public IList<Lines> GetRoute(int departureId, int destinationId)
         {
+            if (departureId == destinationId)
+            {
+                return null; // foutmelding
+            }
+
             RailwayStations london = railwayStationService.Get("Londen");
             RailwayStations amsterdam = railwayStationService.Get("Amsterdam");
             RailwayStations rome = railwayStationService.Get("Rome");
             RailwayStations paris = railwayStationService.Get("Parijs");
             RailwayStations moscow = railwayStationService.Get("Moskou");
+            RailwayStations brussels = railwayStationService.Get("Brussel");
+            RailwayStations berlin = railwayStationService.Get("Berlin");
 
             IList<Lines> route = new List<Lines>();
 
-            if (departureId == london.Id || destinationId == london.Id)
+            if (departureId == london.Id)
             {
+                route.Add(lineDAO.LineWithDepartureAndDestination(departureId, brussels.Id));
+                departureId = brussels.Id;
+            }
+            else if (departureId == moscow.Id)
+            {
+                route.Add(lineDAO.LineWithDepartureAndDestination(departureId, berlin.Id));
+                departureId = brussels.Id;
+            }
 
+            if (destinationId == moscow.Id)
+            {
+                route.Add(lineDAO.LineWithDepartureAndDestination(departureId, berlin.Id));
+                route.Add(lineDAO.LineWithDepartureAndDestination(berlin.Id, destinationId));
+            }
+            else if ((departureId == amsterdam.Id && destinationId == rome.Id) || (departureId == amsterdam.Id && destinationId == paris.Id)
+                || (departureId == rome.Id && destinationId == amsterdam.Id) || (departureId == paris.Id && destinationId == amsterdam.Id)
+                || destinationId == london.Id) 
+            {
+                route.Add(lineDAO.LineWithDepartureAndDestination(departureId, brussels.Id));
+                route.Add(lineDAO.LineWithDepartureAndDestination(brussels.Id, destinationId));
+            }
+            else
+            {
+                route.Add(lineDAO.LineWithDepartureAndDestination(departureId, destinationId));
             }
 
             return route;
