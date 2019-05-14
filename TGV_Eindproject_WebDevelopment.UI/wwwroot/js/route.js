130378c6b51ff196be33e0bd8bc02134b46cc23c
Jochen Schotte <jochen.schotte@student.vives.be>
2019-05-14 22:14:43 +0200

    Start of datepicker element

---
+++ TGV_Eindproject_WebDevelopment.UI/wwwroot/js/route.js
@@ -0,0 +1,11 @@
+﻿$(() => {
+    let dateInput = $("#dateOfDeparture");
+
+    let today = new Date();
+    console.log(today);
+    let finalDate = today.getDate() + 14;
+    console.log(finalDate);
+
+    dateInput.attr("min", today);
+    dateInput.attr("max", finalDate);
+})
