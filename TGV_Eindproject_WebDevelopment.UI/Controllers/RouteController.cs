b3c77e79061c213254f40e3961b84ee5f1d36440
Jochen Schotte <jochen.schotte@student.vives.be>
2019-05-14 21:39:44 +0200

    Added autofill for departure and destination

--- TGV_Eindproject_WebDevelopment.UI/Controllers/RouteController.cs
+++ TGV_Eindproject_WebDevelopment.UI/Controllers/RouteController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using TGV_Eindproject_WebDevelopment.Service;
 
 namespace TGV_Eindproject_WebDevelopment.UI.Controllers
@@ -10,15 +11,18 @@ namespace TGV_Eindproject_WebDevelopment.UI.Controllers
     public class RouteController : Controller
     {
         private readonly TGVService tgvService;
+        private readonly RailwayStationService railwayStationService;
 
         public RouteController()
         {
             tgvService = new TGVService();
+            railwayStationService = new RailwayStationService();
         }
 
         [HttpGet]
         public IActionResult Index()
         {
+            ViewBag.stations = new SelectList(railwayStationService.All(), "Id", "City");
             return View();
         }
 
