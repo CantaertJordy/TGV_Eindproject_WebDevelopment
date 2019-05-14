d4939f78db52404e22115785a3d12848d81cf49f
Jochen Schotte <jochen.schotte@student.vives.be>
2019-05-14 21:08:06 +0200

    Added RouteController and Route/Index view

---
+++ TGV_Eindproject_WebDevelopment.UI/Controllers/RouteController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TGV_Eindproject_WebDevelopment.Service;
+
+namespace TGV_Eindproject_WebDevelopment.UI.Controllers
+{
+    public class RouteController : Controller
+    {
+        private readonly TGVService tgvService;
+
+        public RouteController()
+        {
+            tgvService = new TGVService();
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CalculateRoute()
+        {
+            return View();
+        }
+    }
+}
