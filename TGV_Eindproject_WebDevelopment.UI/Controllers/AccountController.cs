e6d58e009c8214b26e3fab258a599d7326cc9b1c
Jordy Cantaert <jordy.cantaert@student.vives.be>
2019-05-14 20:21:43 +0200

    Account controller - toegevoegd

---
+++ TGV_Eindproject_WebDevelopment.UI/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TGV_Eindproject_WebDevelopment.UI.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
