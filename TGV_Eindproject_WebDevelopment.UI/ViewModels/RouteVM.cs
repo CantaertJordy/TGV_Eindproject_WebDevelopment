c0a46a9ba57290c864e072afa3f5bab8528f0ff3
Jochen Schotte <jochen.schotte@student.vives.be>
2019-05-15 12:11:52 +0200

    Added RouteVM viewmodel

---
+++ TGV_Eindproject_WebDevelopment.UI/ViewModels/RouteVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TGV_Eindproject_WebDevelopment.Domain.Entities;
+
+namespace TGV_Eindproject_WebDevelopment.UI.ViewModels
+{
+    public class RouteVM
+    {
+        public string StartStation { get; set; }
+        public string EndStation { get; set; }
+        public DateTime TimeOfDeparture { get; set; }
+        public DateTime TimeOfArrival { get; set; }
+        public Tgvs Tgv { get; set; }
+    }
+}
