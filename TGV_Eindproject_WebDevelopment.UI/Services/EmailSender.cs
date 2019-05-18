f8bcd61a73aa0a0fde663ca3cab7f5b5bbe60677
Jordy Cantaert <jordy.cantaert@student.vives.be>
2019-05-18 22:16:02 +0200

    Fixes

--- TGV_Eindproject_WebDevelopment.UI/Services/EmailSender.cs
+++ TGV_Eindproject_WebDevelopment.UI/Services/EmailSender.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace TGV_Eindproject_WebDevelopment.UI.Services
 {
-    public class EmailSender
+    public class EmailSender //: IEmailSender
     {
         public async Task SendEmailAsync(string email, string subject, string message)
         {
