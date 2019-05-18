c8c12b42d0804f2de863fc3c5c70ddaec23b4cfa
Jochen Schotte <jochen.schotte@student.vives.be>
2019-05-18 16:23:18 +0200

    Fixed not continuing from Users

--- TGV_Eindproject_WebDevelopment.UI/wwwroot/js/users.js
+++ TGV_Eindproject_WebDevelopment.UI/wwwroot/js/users.js
@@ -1,20 +1,18 @@
 ﻿$(() => {
     $("#submit").click((event) => {
-        event.preventDefault();
-
         let isValid = true;
 
         let i = 0;
-        while (i < $("input").length - 1 && isValid) {
+        while (i < $(".userInput").length && isValid) {
             if ($("#txtName" + i).val().localeCompare("") === 0)
                 isValid = false;
 
             i++;
         }
 
-        if (isValid)
-            $("#userForm").trigger("submit");
-        else
+        if (!isValid) {
             $("#error").text("Make sure you fill in all names.");
+            event.preventDefault();
+        }
     });
 });
